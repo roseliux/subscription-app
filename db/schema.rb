c63545ae494da061e44cbbe0fe983f4c75159644
roseliux <roxdurazo@gmail.com>
2018-04-19 16:13:28 -0700

    Create publication model and controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_180442) do
+ActiveRecord::Schema.define(version: 2018_04_19_230401) do
+
+  create_table "publications", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "file_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

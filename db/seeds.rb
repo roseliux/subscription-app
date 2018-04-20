687210a3aba4418a346b6377d98e5973f6a970f9
roseliux <roxdurazo@gmail.com>
2018-04-19 17:02:09 -0700

    Edit layout, create seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.create()
+puts "Running seeds..."
+
+Publication.all.destroy_all
+
+Publication.create(title: 'first publication',
+  description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+  file_url: 'url.test.com/1')
+
+Publication.create(title: 'second publication',
+  description: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+  file_url: 'url.test.com/2')

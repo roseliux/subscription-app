5e035ab717374b177e1ce0a539051d1ea9620dd9
roseliux <roxdurazo@gmail.com>
2018-04-23 11:01:30 -0700

    Cancelling subscription and Implementing a Subscription Gate.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/users/info', to: 'users#info'
+  post '/users/charge', to: 'users#charge'
+  get '/users/cancel_subscription', to: 'users#cancel_subscription'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'

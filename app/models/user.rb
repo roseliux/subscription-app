24ef26e68daa4325780fd7993d4e387546838345
roseliux <roxdurazo@gmail.com>
2018-04-23 10:23:49 -0700

    Creating the subscription model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :subscription
+
+  after_create :create_subscription
+
+  def create_subscription
+    Subscription.create(user_id: id) if subscription.nil?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_20_170756) do
+ActiveRecord::Schema.define(version: 2018_04_23_172148) do
 
   create_table "publications", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_04_20_170756) do
     t.string "file_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "stripe_user_id"
+    t.boolean "active", default: false, null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
